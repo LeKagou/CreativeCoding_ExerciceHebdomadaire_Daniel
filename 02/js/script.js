893b7a94d2b3121b659b66cef4e8617bd7bb7f09
LeKagou <Daniel.rocha@ecal.ch>
2023-10-19 20:16:46 +0200

    Colors

--- 02/js/script.js
+++ 02/js/script.js
@@ -21,8 +21,30 @@ function createCanvas(w, h) {
 function circle(x, y, rayon) {
   context.beginPath();
   context.arc(x, y, rayon, 0, 2 * Math.PI, true);
-  context.strokeStyle = "hsl(50%, 50%,50%)";
+  var r = centerX + Math.cos(5 * (Math.PI / 180)) * bigRadius;
+  context.strokeStyle = `hsl(50%, 50%,50%)`;
+  context.fill();
   context.stroke();
+  context.closePath();
+}
+
+function rect(x, y, w, h) {
+  context.beginPath();
+  context.rect(x, y, w, h);
+  context.strokeStyle = "hsl(50%, 50%,50%)";
+  context.fillStyle = "rgb(0,0,255)"
+  context.fill();
+  context.closePath();
+}
+
+function triangle(x,y,size){
+  context.beginPath();
+  context.moveTo(x,y);
+  context.lineTo(x + size, y);
+  context.lineTo(x + size, y + size);
+  context.strokeStyle = "hsl(50%, 50%,50%)";
+  context.fillStyle = "rgb(0,255,0)"
+  context.fill()
   context.closePath();
 }
 
@@ -33,8 +55,8 @@ function setup() {
   a1 = 0;
   a2 = 0;
   rayon = 500;
-  bigRadius = 500;
-  smallRadius = 500;
+  bigRadius = 800;
+  smallRadius = 450;
   centerX = width / 2;
   centerY = height / 2;
 
@@ -43,13 +65,14 @@ function setup() {
   draw();
 }
 
-let invertor = 1;
+var r = Math.abs(100 * Math.cos(a1 * (Math.PI / 180)));
+var compteur = 0;
 
 function draw() {
   //   console.log("draw");
-  context.clearRect(0, 0, width, height);
-  context.fillStyle = "rgba(255,255,255)";
-  context.fillRect(0, 0, width, height);
+  //context.clearRect(0, 0, width, height);
+  context.fillStyle = "rgba(0,0,0)";
+  //context.fillRect(0, 0, width, height);
   //
 
   //Si 360 alors on revient à 0 la forme se répète
@@ -63,12 +86,29 @@ function draw() {
   //Mouvement Vericale
   var posy = centerY + Math.sin(a2 * (Math.PI / 180)) * smallRadius;
   //Radius du cercle
-  var r = Math.abs(100 * Math.cos(a1 * (Math.PI / 180)));
 
-  circle(posx, posy, 10);
+  if(compteur == 0)
+  {
+    compteur = 1;
+  }
+  /*else if(compteur == 1)
+  {
+    rect(posx,posy,r,r)
+    compteur = 2;
+  }
+  else if(compteur == 2){
+    triangle(posx,posy,r);
+        compteur = 0;
+  }*/
+
+  context.fillStyle = colorC
+  circle(posx, posy, r);
+
 
   requestAnimationFrame(draw);
 }
+
+var colorC = `rgb(0,0,255)`
 
 
 function randomIntFromInterval(min, max) { // min and max included 
@@ -77,10 +117,14 @@ function randomIntFromInterval(min, max) { // min and max included
 
 var rndInt = randomIntFromInterval(1, 6)
 var rndInt2 = randomIntFromInterval(1, 6)
+var rInt2 = randomIntFromInterval(1, 6)
 
 function timing(){
-  rndInt = randomIntFromInterval(1, 6)
+  rndInt = randomIntFromInterval(-6, 6)
   rndInt2 = randomIntFromInterval(1,6)
+  rInt2 = randomIntFromInterval(1, 50)
+  colorC = `rgb(${randomIntFromInterval(0,255)},${randomIntFromInterval(0,255)},${randomIntFromInterval(0,255)})`
+  r = rInt2;
 }
 
 function mousePressed(e) {
