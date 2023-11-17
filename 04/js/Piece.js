831a6d9085b98421dc919d46cfae474e6b060c10
LeKagou <Daniel.rocha@ecal.ch>
2023-11-17 21:10:06 +0100

    Update

--- 04/js/Piece.js
+++ 04/js/Piece.js
@@ -1,26 +1,86 @@
 class Piece {
-  constructor(x, y, color, context) {
+  constructor(x, y, r, color,main, context) {
     this.context = context;
     this.x = x;
     this.y = y;
     this.color = color;
+    this.ActualR = 0;
+    this.r = r;
+    this.main = main;
+    this.unlockCheck = false;
+    this.rot;
   }
 
-  draw(r, rot, vw, vh, main) {
+  draw(vw, vh) {
     //Placement
     this.context.save();
-    rot = rot * (Math.PI / 180);
+    let rotConverted = this.rot * (Math.PI / 180);
     this.context.translate(vw, vh);
-    this.context.rotate(rot);
+    this.context.rotate(rotConverted);
     //Dessiner les formes + attribuer la couleur
     this.context.fillStyle = this.color;
-    this.createCircle(0, 0, r);
+    this.createCircle(0, 0, this.r);
     this.context.fillStyle = "rgba(0,0,0)";
-    this.createRect(100, r * 2 - 20);
+    this.createRect(100, this.r * 2 - 20);
     //Dessiner les yeux
-    this.oeil(60, main);
+    this.oeil(60, this.main);
     this.context.restore();
+    this.ActualR = this.r;
   }
+
+  addRotation(){
+    this.rot += 10;
+    if(this.rot>=360)
+    {
+      this.rot = 0;
+    }
+    console.log(this.rot);
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  changeColor(){
+    if(this.unlockCheck == true)
+    {
+      this.color = "#62e379";
+    }
+    else
+    {
+      this.color = "#ff6390";
+    }
+  }
+
+  isInMe(mouseX, mouseY) {
+    // on calcule la distance entre la souris et le centre
+    let d = this.dist(mouseX, mouseY, this.x + window.innerWidth / 2, this.y + window.innerHeight / 2);
+    console.log(d);
+    console.log(this.x + window.innerWidth / 2,this.y + window.innerHeight / 2);
+    // on compare cette distance au rayon
+    if(!this.main){
+      if (d < this.r && d >= this.r - 100) {
+        return true;
+      } else {
+        return false;
+      }
+    }else{
+      if (d < this.r) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  dist(x1, y1, x2, y2) {
+    // calcule la distance entre deux points
+    // pythagore power
+    let d = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    return d;
+  }
+
+
 
   //#region Formes Principales
   createCircle(x, y, r) {
--- 04/js/script.js
+++ 04/js/script.js
@@ -17,7 +17,7 @@ function createCanvas(w, h) {
 }
 
 function draw() {
-  // console.log("draw");
+  console.log("draw");
   context.fillStyle = "rgba(235,235,235)";
   //context.fillRect(0, 0, width, height);
   //monCercle.draw();
@@ -27,9 +27,11 @@ function draw() {
   oeil();
   requestAnimationFrame(draw);*/
 
-  piece.draw(350,90,width/2,height/2, false);
-  piece.draw(250,90,width/2,height/2, false);
-  piece.draw(150,90,width/2,height/2, true);
+  piece3.draw(width/2,height/2);
+  piece2.draw(width/2,height/2);
+  piece1.draw(width/2,height/2);
+  
+  requestAnimationFrame(draw);
 }
 
 
@@ -37,8 +39,15 @@ function setup() {
   console.log("setup");
   createCanvas(width, height);
   //monCercle = new Circle(400, 400, 100, context);
-  piece = new Piece(0, 0,"#ff6390",context);
+  piece3 = new Piece(0, 0,350,"#ff6390", false,context);
+  piece2 = new Piece(0, 0,250,"#ff6390", false,context);
+  piece1 = new Piece(0, 0,150,"#ff6390", true,context);
   document.addEventListener("click", mousePressed);
+
+  piece3.rot = piece3.getRandomInt(35) * 10;
+  piece2.rot = piece2.getRandomInt(35) * 10;
+  piece1.rot = piece1.getRandomInt(35) * 10;
+
   draw();
 }
 
@@ -47,6 +56,46 @@ function mousePressed(e) {
   //monCercle.changeColor();
   //monCercle.definirDestination(e.x, e.y);
   //monCercle.definirRayonAleatoire();
+  //console.log(e.x, e.y);
+  /*if(e.x < 150 && e.y < 150){
+    piece1.isInMe(e.x, e.y);
+  }
+  if(e.x > 150 && e.x < 250 && e.y > 150 && e.y < 250){
+    piece2.isInMe(e.x, e.y);
+  }*/
+  if(piece3.isInMe(e.x, e.y) == true){
+    console.log("true");
+    piece3.addRotation();
+  }
+
+  if(piece2.isInMe(e.x, e.y) == true){
+    console.log("true");
+    piece2.addRotation();
+  }
+
+  if(piece1.isInMe(e.x, e.y) == true){
+    console.log("true");
+    piece1.addRotation();
+  }
+
+  if(piece1.rot == piece2.rot && piece1.rot == piece3.rot){
+    piece3.unlockCheck = true;
+    piece2.unlockCheck = true;
+    piece1.unlockCheck = true;
+    piece3.changeColor();
+    piece2.changeColor();
+    piece1.changeColor();
+  }
+  else
+  {
+    piece3.unlockCheck = false;
+    piece2.unlockCheck = false;
+    piece1.unlockCheck = false;
+    piece3.changeColor();
+    piece2.changeColor();
+    piece1.changeColor();
+  }
+
 }
 
 window.onload = function () {
