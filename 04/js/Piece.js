45ed5bfb12e00371b14372d9af51d17fe4c7f233
LeKagou <Daniel.rocha@ecal.ch>
2023-11-19 19:52:12 +0100

    ColorFade

--- 04/js/Piece.js
+++ 04/js/Piece.js
@@ -8,7 +8,19 @@ class Piece {
     this.r = r;
     this.main = main;
     this.unlockCheck = false;
+
+    this.randomAxe = 20;
     this.rot;
+
+    //Animation
+    this.timing = 0;
+    this.timing2 = 1;
+    this.savedRot = this.rot;
+
+    this.red = 255;
+    this.green = 99;
+    this.blue = 144;
+
   }
 
   draw(vw, vh) {
@@ -26,37 +38,99 @@ class Piece {
     this.oeil(60, this.main);
     this.context.restore();
     this.ActualR = this.r;
+
+    this.animateRotation();
+    this.changeColorAnimated();
+
   }
 
   addRotation(){
-    this.rot += 10;
-    if(this.rot>=360)
-    {
-      this.rot = 0;
-    }
+    this.timing = 0;
+    this.savedRot += 20;
     console.log(this.rot);
   }
 
+  animateRotation(){
+    if(this.rot >= this.savedRot)
+    {
+      return;
+    }
+    const easing = Easing.circInOut(this.timing);
+    this.timing += 0.01;
+
+    this.rot = this.rot + 20 * easing;
+  }
+
   getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    this.rot = 0;
+    let r = Math.floor(Math.random() * max);
+    if(r >= 180)
+    {
+      r -= 180;
+    }
+    this.savedRot = r;
+    return r
   }
 
   changeColor(){
-    if(this.unlockCheck == true)
+
+    if(this.rot + 20 >= this.randomAxe -1 && this.rot + 20 <= this.randomAxe + 1)
+    {
+      this.unlockCheck = true;
+      this.timing2 = 0;
+      console.log(this.unlockCheck);
+      console.log("Passe");
+    }
+    else
+    {
+      if(this.unlockCheck == true)
+      {
+        this.timing2 = 0;
+        console.log("Zero");
+      }
+      this.unlockCheck = false;
+      console.log("PassePAS");
+    }
+
+    /*if(this.unlockCheck == true)
     {
       this.color = "#62e379";
     }
     else
     {
       this.color = "#ff6390";
+    }*/
+  }
+
+  changeColorAnimated(){
+    const easing = Easing.cubicIn(this.timing2);
+
+    //Rouge 255,99,144 Vert 98,227,121
+    let diffRed = this.red - 98;
+    let diffGreen = 227 - this.green;
+    let diffBlue = this.blue - 121;
+
+    //console.log(this.unlockCheck);
+    
+    if(this.timing2 >= 1)
+    {
+      //console.log("return");
+      return;
+    }
+    this.timing2 += 0.07;
+    if(this.unlockCheck == true)
+    {
+      this.color = `rgba(${this.red - diffRed * easing},${this.green + diffGreen * easing},${this.blue - diffBlue * easing})`;
+    }
+    else
+    {
+      this.color = `rgba(${98 + diffRed * easing},${227 - diffGreen * easing},${121 + diffBlue * easing})`;
     }
   }
 
   isInMe(mouseX, mouseY) {
     // on calcule la distance entre la souris et le centre
     let d = this.dist(mouseX, mouseY, this.x + window.innerWidth / 2, this.y + window.innerHeight / 2);
-    console.log(d);
-    console.log(this.x + window.innerWidth / 2,this.y + window.innerHeight / 2);
     // on compare cette distance au rayon
     if(!this.main){
       if (d < this.r && d >= this.r - 100) {
--- 04/js/script.js
+++ 04/js/script.js
@@ -19,14 +19,6 @@ function createCanvas(w, h) {
 function draw() {
   console.log("draw");
   context.fillStyle = "rgba(235,235,235)";
-  //context.fillRect(0, 0, width, height);
-  //monCercle.draw();
-  /*anneau(20 * 360,400)
-  anneau(10 * 360,300)
-  anneauYeux(180,200)
-  oeil();
-  requestAnimationFrame(draw);*/
-
   piece3.draw(width/2,height/2);
   piece2.draw(width/2,height/2);
   piece1.draw(width/2,height/2);
@@ -38,64 +30,35 @@ function draw() {
 function setup() {
   console.log("setup");
   createCanvas(width, height);
-  //monCercle = new Circle(400, 400, 100, context);
   piece3 = new Piece(0, 0,350,"#ff6390", false,context);
   piece2 = new Piece(0, 0,250,"#ff6390", false,context);
   piece1 = new Piece(0, 0,150,"#ff6390", true,context);
   document.addEventListener("click", mousePressed);
 
-  piece3.rot = piece3.getRandomInt(35) * 10;
-  piece2.rot = piece2.getRandomInt(35) * 10;
-  piece1.rot = piece1.getRandomInt(35) * 10;
+  piece3.rot = piece3.getRandomInt(0) * 10;
+  piece2.rot = piece2.getRandomInt(0) * 10;
+  piece1.rot = piece1.getRandomInt(0) * 10;
 
   draw();
 }
 
 function mousePressed(e) {
   console.log("mousePressed", e);
-  //monCercle.changeColor();
-  //monCercle.definirDestination(e.x, e.y);
-  //monCercle.definirRayonAleatoire();
-  //console.log(e.x, e.y);
-  /*if(e.x < 150 && e.y < 150){
-    piece1.isInMe(e.x, e.y);
-  }
-  if(e.x > 150 && e.x < 250 && e.y > 150 && e.y < 250){
-    piece2.isInMe(e.x, e.y);
-  }*/
+
   if(piece3.isInMe(e.x, e.y) == true){
-    console.log("true");
     piece3.addRotation();
+    piece3.changeColor();
   }
 
   if(piece2.isInMe(e.x, e.y) == true){
-    console.log("true");
     piece2.addRotation();
+    piece2.changeColor();
   }
 
   if(piece1.isInMe(e.x, e.y) == true){
-    console.log("true");
     piece1.addRotation();
-  }
-
-  if(piece1.rot == piece2.rot && piece1.rot == piece3.rot){
-    piece3.unlockCheck = true;
-    piece2.unlockCheck = true;
-    piece1.unlockCheck = true;
-    piece3.changeColor();
-    piece2.changeColor();
     piece1.changeColor();
   }
-  else
-  {
-    piece3.unlockCheck = false;
-    piece2.unlockCheck = false;
-    piece1.unlockCheck = false;
-    piece3.changeColor();
-    piece2.changeColor();
-    piece1.changeColor();
-  }
-
 }
 
 window.onload = function () {
